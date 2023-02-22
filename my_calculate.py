4425726ff135a8c3343de178c321731e0a9156fa
Juli Tay <119788019+julitays@users.noreply.github.com>
2023-02-22 22:58:47 +0300

    fix mistakes

--- my_calculate.py
+++ my_calculate.py
@@ -2,43 +2,37 @@
 
 print('Hello, how are you? I want to show you my new calculator. Are you going to try it?')
 
-x, y = int(input('Введите число 1: ')), int(input('Введите число 2: '))
+x, y = map(int, input('Введите два числа: ').split())
 
 
 while True:
-    oper = input('Введите операцию: ')
+    oper = input('Введите операцию, 'stop' или 'x' для выхода: ')
     if oper == '+':
-        print(x+y)
+        print(x + y)
 
     elif oper == '-':
-        print(x-y)
+        print(x - y)
 
     elif oper == '*':
-        print(x*y)
+        print(x * y)
 
     elif oper == '/':
         if x == 0 or y == 0:
             print('На ноль делить нельзя, как и ноль, собственно, на что-то тоже!')
         else:
             print(x / y)
-
     elif oper == '%':
-        print(x%y)
-
+        print(x % y)
     # Остаток от деления
-
-    elif oper == '//':
+    elif oper == '//'':
         if x == 0 or y == 0:
             print('На ноль делить нельзя, как и ноль, собственно, на что-то тоже!')
         else:
             print(x // y)
-
     # Целочисленное деление
-
     elif oper == '**':
-        print(x**y)
-
-    elif oper == 'stop':
+        print(x ** y)
+    elif oper == 'stop' or 'x':
         break
     else:
         print('Введите операцию!')
